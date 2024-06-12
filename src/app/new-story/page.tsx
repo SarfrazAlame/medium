9b0dbfd2ff7a4e41e86008bb41a4a452531e75bd
Sarfraz Alam <sarfrazk7858@gmail.com>
2024-06-12 15:53:01 +0530

    fomr

--- src/app/new-story/page.tsx
+++ src/app/new-story/page.tsx
@@ -1,7 +1,79 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
 import React from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const formSchema = z.object({
+  title: z.string().min(1).max(20),
+  story: z.string().max(250),
+});
 
 const page = () => {
-  return <></>;
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+      story: "",
+    },
+  });
+
+  const { isValid, isSubmitting } = form.formState;
+
+  const handleSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+  return (
+    <>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(handleSubmit)}
+          className="flex flex-col gap-4 my-12 "
+        >
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem className="flex w-full justify-center">
+                <FormControl>
+                  <Input
+                    placeholder="Title"
+                    {...field}
+                    className="w-full h-full md:w-1/3 lg:text-5xl text-3xl text-gray-500 placeholder:text-zinc-400 placeholder:font-serif"
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="story"
+            render={({ field }) => (
+              <FormItem className="flex w-full justify-center">
+                <FormControl>
+                  <Input
+                    placeholder="Tell your story..."
+                    {...field}
+                    className="w-full h-full md:w-1/3 text-xl text-gray-500 placeholder:text-zinc-400 placeholder:font-serif"
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <Button
+          type="submit"
+            disabled={!isValid || isSubmitting}
+            className="absolute right-1/3 border top-6 bg-green-800 rounded-full text-gray-50 text-[15px]"
+          >
+            Publish
+          </Button>
+        </form>
+      </Form>
+    </>
+  );
 };
 
 export default page;
--- src/components/WriteHeader.tsx
+++ src/components/WriteHeader.tsx
@@ -1,19 +1,20 @@
 import { UserButton } from "@clerk/nextjs";
-import { DotSquare } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 import { BsThreeDots } from "react-icons/bs";
-import { GrThreeD } from "react-icons/gr";
 import { PiBellThin } from "react-icons/pi";
 
 const WriteHeader = () => {
   return (
     <div className="w-full">
       <div className=" w-1/2  h-20 flex justify-between items-center mx-auto">
-        <div className="flex items-center gap-2">
-          <Image src={"/logoipsum-274.svg"} height={45} width={45} alt="" />
-          <p className="text-sm text-slate-600">Draft</p>
-        </div>
+        <Link href={"/"}>
+          <div className="flex items-center gap-2 mt-1">
+            <Image src={"/logoipsum-274.svg"} height={45} width={45} alt="" />
+            <p className="text-sm text-slate-600">Draft</p>
+          </div>
+        </Link>
         <div className="flex items-center gap-5">
           <BsThreeDots className="cursor-pointer" />
           <PiBellThin size={24} className="cursor-pointer" />
