f397f6916e56274aa4402ae1cd3dcfe8ebda1bce
Sarfraz Alam <sarfrazk7858@gmail.com>
2024-06-18 10:13:40 +0530

    update .env

--- src/components/WriteHeader.tsx
+++ src/components/WriteHeader.tsx
@@ -1,3 +1,4 @@
+import { getAuthOptions } from "@/auth/auth";
 import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
@@ -5,11 +6,13 @@ import React from "react";
 import { BsThreeDots } from "react-icons/bs";
 import { PiBellThin } from "react-icons/pi";
 
-const WriteHeader = () => {
+const WriteHeader = async () => {
+  const session = await getAuthOptions();
+  const user = session?.user;
   return (
     <div className="w-full">
       <div className=" w-1/2  h-20 flex justify-between items-center mx-auto">
-        <Link href={"/"}>
+        <Link href={"/dashboard"}>
           <div className="flex items-center gap-2 mt-1">
             <Image src={"/logoipsum-274.svg"} height={45} width={45} alt="" />
             <p className="text-sm text-slate-600">Draft</p>
@@ -18,7 +21,7 @@ const WriteHeader = () => {
         <div className="flex items-center gap-5">
           <BsThreeDots className="cursor-pointer" />
           <PiBellThin size={24} className="cursor-pointer" />
-          <UserButton />
+          <Image src={user?.image!} alt="" width={40} height={40} className="rounded-full" />
         </div>
       </div>
     </div>
